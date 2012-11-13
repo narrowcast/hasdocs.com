ed2b65ce449a9021ea0c989cfc69c62841cf4a1a
Chee-Hyung Yoon <yoonchee@gmail.com>
2012-11-13 13:59:56 +0900

    Added Plan model to the hasdocs.accounts app.

--- hasdocs/accounts/admin.py
+++ hasdocs/accounts/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from hasdocs.accounts.models import UserProfile, UserType
+from hasdocs.accounts.models import Plan, UserProfile, UserType
 
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'private_docs', 'business')
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type', 'company', 'location')
+
+admin.site.register(Plan, PlanAdmin)
 admin.site.register(UserType)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, UserProfileAdmin)
--- hasdocs/accounts/models.py
+++ hasdocs/accounts/models.py
@@ -3,6 +3,20 @@ from django.db import models
 from django.db.models.signals import post_save
 
 
+class Plan(models.Model):
+    """Model for a plan."""
+    # Name of the plan
+    name = models.CharField(max_length=50)
+    # Number of private docs
+    private_docs = models.PositiveIntegerField()
+    # Monthly price of the plan
+    price = models.DecimalField(max_digits=64, decimal_places=2, default=0)
+    # Whether this is a plan for an organization rather than uesr
+    business = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
+
 class UserType(models.Model):
     """Type of a user (e.g. individual user or organization)."""
     # Name of the user type
@@ -14,11 +28,11 @@ class UserType(models.Model):
 class UserProfile(models.Model):
     """Model for representing user profiles."""
     # One-to-one mapping to the auth user model
-    user = models.OneToOneField(User)    
+    user = models.OneToOneField(User)
     # Blog or webiste URL
     url = models.URLField(blank=True)
     # Company
-    company = models.CharField(max_length=50, blank=True)    
+    company = models.CharField(max_length=50, blank=True)
     # Location
     location = models.CharField(max_length=50, blank=True)
     # User ype (e.g., user or organization)

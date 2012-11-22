c00b9342882b6b0626567ca69a6c0a039cfb073d
Chee-Hyung Yoon <yoonchee@gmail.com>
2012-11-22 10:06:12 +0900

    Added Domain to the projects admin.

--- hasdocs/projects/admin.py
+++ hasdocs/projects/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from hasdocs.projects.models import Generator, Language, Project
+from hasdocs.projects.models import Domain, Generator, Language, Project
 
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('owner', 'name', 'language', 'generator', 'url', 'description')
 
+admin.site.register(Domain)
 admin.site.register(Generator)
 admin.site.register(Language)
 admin.site.register(Project, ProjectAdmin)

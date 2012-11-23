4dfdd3381b0de6a606587b9917eca7b90a4de208
Chee-Hyung Yoon <yoonchee@gmail.com>
2012-11-23 17:56:42 +0900

    Implemented detection of already imported GitHub or Heroku projects.

--- hasdocs/accounts/forms.py
+++ hasdocs/accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.forms.widgets import PasswordInput
 
+from hasdocs.accounts.models import UserProfile
+
 
 class SignupForm(forms.ModelForm):
     """Form for signing up."""
@@ -11,7 +13,13 @@ class SignupForm(forms.ModelForm):
         widgets = {'password': PasswordInput}
         
 class UserUpdateForm(forms.ModelForm):
-    """Form for updating user settings."""    
+    """Form for updating user settings."""
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
+
+class HerokuConnectForm(forms.ModelForm):
+    """Form for setting Heroku api key."""
+    class Meta:
+        model = UserProfile
+        fields = ('heroku_api_key', )
--- hasdocs/projects/views.py
+++ hasdocs/projects/views.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import requests
 
@@ -47,6 +48,11 @@ class ProjectUpdateView(UpdateView):
         if request.user != project.owner:
             raise Http404
         return super(ProjectUpdateView, self).dispatch(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        """Logs updating of the project."""
+        logger.info('Updated project %s' % self.kwargs['slug'])
+        return super(ProjectUpdateView, self).form_valid(form)
 
 class ProjectDeleteView(DeleteView):
     """View for delting a project."""
@@ -59,6 +65,11 @@ class ProjectDeleteView(DeleteView):
         if request.user != project.owner:
             raise Http404
         return super(ProjectDeleteView, self).dispatch(request, *args, **kwargs)
+    
+    def get_success_url(self):
+        """Returns the URL of the user's detail_view."""
+        logger.info('Deleted project %s' % self.kwargs['slug'])
+        return self.request.user.get_absolute_url()
 
 class GitHubProjectListView(TemplateView):
     """View for viewing the list of GitHub projects."""
@@ -79,7 +90,11 @@ class GitHubProjectListView(TemplateView):
         payload = {'access_token': access_token }
         r = requests.get('%s/user/repos' % settings.GITHUB_API_URL,
                          params=payload)
-        context['repos'] = r.json
+        repos = r.json
+        # Mark already imported repos
+        for repo in repos:
+            repo['exists'] = Project.objects.filter(url=repo['html_url']).exists()
+        context['repos'] = repos
         return context
 
 class HerokuProjectListView(TemplateView):
@@ -100,39 +115,66 @@ class HerokuProjectListView(TemplateView):
         api_key = self.request.user.get_profile().heroku_api_key
         r = requests.get('%s/apps' % settings.HEROKU_API_URL,
                          auth=('', api_key))
-        context['apps'] = r.json
+        apps = r.json
+        # Mark already imported apps
+        for app in apps:
+            app['exists'] = Project.objects.filter(url=app['web_url']).exists()
+        context['apps'] = apps
         return context
     
 def import_from_github(request):
-    """Imports a project from GitHub repository."""
+    """Imports a project from a GitHub repository."""
     if request.method == 'POST':
         logger.info('Importing a repository from GitHub')
         access_token = request.user.get_profile().github_access_token
         payload = {'access_token': access_token }
-        r = requests.get(
-            '%s/repos/%s/%s' % (settings.GITHUB_API_URL,request.POST['owner'],
-                                request.POST['repo']), params=payload)
+        r = requests.get('%s/repos/%s/%s' % (
+            settings.GITHUB_API_URL,request.POST['owner'], request.POST['repo']
+        ), params=payload)
         repo = r.json
-        owner = User.objects.get(username=repo['owner']['login'])
         # Creates a new project based on the GitHub repo
-        project = Project(owner=owner, name=repo['name'], url=repo['html_url'],
+        project = Project(owner=request.user, name=repo['name'],
+                          description=repo['description'], url=repo['html_url'],
                           git_url=repo['git_url'], private=repo['private'],
-                          language=repo['language'],
-                          description=repo['description'])
+                          language=repo['language'])
         project.save()
+        logger.info('Imported %s repo from GitHub.' % project.name)
         return HttpResponseRedirect(
-            reverse('project_detail', args=[owner, project]))
+            reverse('project_detail', args=[request.user, project]))
     else:
         raise Http404
     
 def import_from_heroku(request):
-    """Imports a project from Heroku app."""
-    logger.info('Importing an app from Heroku.')
-    api_key = request.user.get_profile().heroku_api_key
-    r = requests.get('%s/apps/%s' % (
-        settings.HEROKU_API_URL, request.POST['app_name']), auth=('', api_key))
-    app = r.json
-    # Creates a new project based on the Heroku app
-    project = Project(name=app['name'], url=app['web_url'],
-                      git_url=app['git_url'], private=True)
-    project.save()
+    """Imports a project from a Heroku app."""
+    if request.method == 'POST':
+        logger.info('Importing an app from Heroku')
+        api_key = request.user.get_profile().heroku_api_key
+        r = requests.get('%s/apps/%s' % (
+            settings.HEROKU_API_URL, request.POST['app_name']
+        ), auth=('', api_key))
+        app = r.json
+        # Creates a new project based on the Heroku app
+        project = Project(owner=request.user, name=app['name'],
+                          url=app['web_url'], git_url=app['git_url'],
+                          private=True)
+        project.save()
+        logger.info('Imported %s app from Heroku.' % project.name)
+        return HttpResponseRedirect(
+            reverse('project_detail', args=[request.user, project]))
+    else:
+        raise Http404
+    
+def create_hook_github(request):
+    """Creates a post-receive hook for the given project at the GitHub repo."""
+    logger.info('Creating a post-receive hook at GitHub')
+    access_token = request.user.get_profile().github_access_token
+    config = {'url': 'http://www.hasdocs.com/post-receive/github/',
+              'content_type': 'json'}
+    payload = {'access_token': access_token, 'name': 'web', 'config': config}
+    r = requests.post('%s/repos/%s/%s/hooks' % (
+        settings.GITHUB_API_URL, request.POST['owner'], request.POST['repo']
+    ), data=json.dumps(payload))
+
+def create_hook_heroku(request):
+    """Creates a post-receive hook for the given project at the Heroku repo."""
+    logger.info('Creating a post-receive hook at Heroku')

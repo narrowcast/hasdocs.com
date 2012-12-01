60e6a48e9879b269bed481fc8c20cf5e8e44ad81
Chee-Hyung Yoon <yoonchee@gmail.com>
2012-12-01 17:06:10 +0900

    Disabled middlewares other than CommonMiddleware for debugging.

--- hasdocs/settings.py
+++ hasdocs/settings.py
@@ -98,13 +98,13 @@ CACHES = {
 }
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.gzip.GZipMiddleware',
-    'hasdocs.core.middleware.SubdomainMiddleware',
+    #'django.middleware.gzip.GZipMiddleware',
+    #'hasdocs.core.middleware.SubdomainMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    #'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )

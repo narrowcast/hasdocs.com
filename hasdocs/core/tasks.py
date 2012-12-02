8127ffdfc1aa4b2f0f234a7c20639d354acb8992
Chee-Hyung Yoon <yoonchee@gmail.com>
2012-12-02 13:20:38 +0900

    Fixed a typo in the initialization of celery logger.

--- hasdocs/core/tasks.py
+++ hasdocs/core/tasks.py
@@ -14,7 +14,7 @@ from django.conf import settings
 from django.core.cache import cache
 from django.core.files import File
 
-logger = logging.get_task_logger(__name__)
+logger = get_task_logger(__name__)
 
 docs_storage = S3BotoStorage(bucket=settings.AWS_DOCS_BUCKET_NAME, acl='private',
                              reduced_redundancy=True, secure_urls=False)

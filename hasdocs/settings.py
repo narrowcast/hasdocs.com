6356809dda1385a0be890c222b2b3f9c9e1e141a
Chee-Hyung Yoon <yoonchee@gmail.com>
2012-12-22 10:07:41 +0900

    Enabled subdomain middleware.

--- hasdocs/settings.py
+++ hasdocs/settings.py
@@ -99,11 +99,11 @@ CACHES = {
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.gzip.GZipMiddleware',
-    #'hasdocs.core.middleware.SubdomainMiddleware',
+    'hasdocs.core.middleware.SubdomainMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
     'hasdocs.accounts.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
